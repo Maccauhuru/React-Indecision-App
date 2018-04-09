06a1e090b1e8689cc59b1626ae2d2a7551251f19
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-04-09 17:30:24 -0500

    widget added

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -70,8 +70,10 @@ export default class IndecisionApp extends React.Component {
         <Header subtitle={subtitle} />
         <div className='container'>
           <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
+          <div className="widget">
           <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
           <AddOption handleAddOption={this.handleAddOption} />
+          </div>
         </div>
       </div>;
   }
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = props =>
-  (
-    <div>
-      <button onClick={props.handleDeleteOptions} className='button button--link'>Remove All</button>
-      {props.options.length === 0 && <p>Please Enter Items To Get Started</p>}
-      {props.options.map(option => (
-        <Option
-          key={option}
-          optionText={option}
-          handleDeleteOption={props.handleDeleteOption}
-        />
-      ))}
+const Options = props => (
+  <div>
+    <div className="widget-header">
+      <h3 className='widget-header__title'>Your Options</h3>
+      <button
+        onClick={props.handleDeleteOptions}
+        className="button button--link"
+      >
+        Remove All
+      </button>
     </div>
-  );
+    {props.options.length === 0 && <p className="widget__message">Please Enter Items To Get Started</p>}
+    {props.options.map(option => (
+      <Option
+        key={option}
+        optionText={option}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
+  </div>
+);
 export default Options;
