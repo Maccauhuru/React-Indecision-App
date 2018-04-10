fb013796f7d4d8be4edcc20fbaf5aa8be16353c6
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-04-10 11:57:28 -0500

    add-option scss & add-option error

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -16,10 +16,10 @@ export default class AddOption extends React.Component {
  
   render() {
     return (
-      <div className="add-option">
+      <div>
         {this.state.error && <p className="add-option-error">{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
           <button className='button'>Add Option</button>
         </form>
       </div>
